f581f5d07c22c3bd002905d7e6fdc5317a8b69d8
Sam Bradley <2729175B@student.gla.ac.uk>
2023-08-04 14:30:01 +0100

    Configuration working, still to fix books.

--- src/main/java/me/samstarplugins/enchantablemending/EnchantableMending.java
+++ src/main/java/me/samstarplugins/enchantablemending/EnchantableMending.java
@@ -1,43 +1,84 @@
 package me.samstarplugins.enchantablemending;
 
+
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.enchantment.EnchantItemEvent;
 import org.bukkit.inventory.ItemStack;
+import java.util.Random;
+
 
 
 public final class EnchantableMending extends JavaPlugin implements Listener {
+
+    private static final int MAX_LEVEL = 30;
+
+    private boolean pluginEnabled;
+    private boolean onlyMaxLvlEnchants;
+    private int mendingChance;
 
     @Override
     public void onEnable() {
         loadConfig();
         // Register the event listener
         getServer().getPluginManager().registerEvents(this, this);
-        getLogger().info("Plugin enabled successfully!");
+
+        getLogger().info("EnchantableMending enabled successfully!");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("Plugin disabled.");
+        getLogger().info("EnchantableMending disabled.");
     }
 
     @EventHandler
     public void onEnchantItem(EnchantItemEvent event) {
-        // Get the item being enchanted
+
+        Random random = new Random();
+        int randomNumber = random.nextInt(100);
+        getLogger().info("Random number: ");
+        getLogger().info(String.valueOf(randomNumber));
+
         ItemStack item = event.getItem();
 
+        getLogger().info("Xp Cost: ");
+        getLogger().info(String.valueOf(event.getExpLevelCost()));
 
-        item.addEnchantment(Enchantment.MENDING, 1);
+        getLogger().info("Mending Chance:");
+        getLogger().info(String.valueOf(mendingChance));
+
+        if (event.getExpLevelCost() < MAX_LEVEL && onlyMaxLvlEnchants)
+        {
+            return;
+        }
+        if (randomNumber < mendingChance) {
+            item.addEnchantment(Enchantment.MENDING, 1);
+        }
+
+
+
+
     }
 
     public void loadConfig()
     {
         saveDefaultConfig();
-        boolean pluginEnabled = getConfig().getBoolean("enable-plugin");
-        boolean onlyMaxLvlEnchants = getConfig().getBoolean("only-max-level-enchants");
-        int mendingChance = getConfig().getInt("mending-enchantment-percent-chance");
+
+        pluginEnabled = getConfig().getBoolean("enable-plugin", true);
+
+        if (!pluginEnabled) {
+            // Disables the plugin if the setting is false
+            getLogger().info("EnchantableMending disabled by config");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        onlyMaxLvlEnchants = getConfig().getBoolean("only-max-level-enchants", true);
+        mendingChance = getConfig().getInt("mending-enchantment-percent-chance", 5);
+
+
     }
 
 }
