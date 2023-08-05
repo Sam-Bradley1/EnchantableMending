b4fe6630fa1c610a3d174387efa95d22885d19ad
Sam Bradley <2729175B@student.gla.ac.uk>
2023-08-06 00:52:21 +0100

    Added option to enable/disable mending and infinity compatibility

--- src/main/java/me/samstarplugins/enchantablemending/EnchantableMending.java
+++ src/main/java/me/samstarplugins/enchantablemending/EnchantableMending.java
@@ -1,6 +1,5 @@
 package me.samstarplugins.enchantablemending;
 
-
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -17,28 +16,33 @@ public final class EnchantableMending extends JavaPlugin implements Listener {
 
     private boolean onlyMaxLvlEnchants;
     private int mendingChance;
+    private boolean infinityMendingAllowed;
 
     @Override
     public void onEnable() {
         loadConfig();
         getServer().getPluginManager().registerEvents(this, this);
-        getLogger().info("[EnchantableMending] Plugin enabled successfully!");
+        getLogger().info("Plugin enabled successfully!");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("[EnchantableMending] Plugin disabled.");
+        getLogger().info("Plugin disabled.");
     }
 
     @EventHandler
     public void onEnchantItem(EnchantItemEvent event) {
 
         Random random = new Random();
+
         int randomNumber = random.nextInt(100);
 
         ItemStack item = event.getItem();
 
         if (event.getExpLevelCost() < MAX_LEVEL && onlyMaxLvlEnchants) {
+            return;
+        }
+        if (item.containsEnchantment(Enchantment.ARROW_INFINITE) && !infinityMendingAllowed) {
             return;
         }
         if (randomNumber < mendingChance) {
@@ -54,13 +58,14 @@ public final class EnchantableMending extends JavaPlugin implements Listener {
         boolean pluginEnabled = getConfig().getBoolean("enable-plugin", true);
 
         if (!pluginEnabled) {
-            getLogger().info("[EnchantableMending] Plugin disabled by configuration");
+            getLogger().info("Plugin disabled by configuration");
             getServer().getPluginManager().disablePlugin(this);
             return;
         }
 
         onlyMaxLvlEnchants = getConfig().getBoolean("only-max-level-enchants", true);
-        mendingChance = getConfig().getInt("mending-enchantment-percent-chance", 5);
+        mendingChance = getConfig().getInt("mending-enchantment-percent-chance", 10);
+        infinityMendingAllowed = getConfig().getBoolean("allow-mending-infinity-enchant", true);
     }
 
 }
